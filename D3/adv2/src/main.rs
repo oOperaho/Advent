72d622935b5c3678fc7dbdf93f89a2b727ad0356
Operaho <emptyloperaho@gmail.com>
2022-05-30 23:38:50 -0300

    no

--- D3/adv2/src/main.rs
+++ D3/adv2/src/main.rs
@@ -1,18 +1,32 @@
-// Considering that 010100010100 and 101011101011 are the two binary criteria
-// results, ill based the code on this
-
 use std::fs;
 
 fn main() {
     let file = fs::read_to_string("/home/operaho/Advent/D3/adv.txt").unwrap();
-    let v = Vec::new();
+    let mut c = 1;
+    let mut v0 = Vec::new();
+    let mut v1 = Vec::new();
 
     for i in file.lines() {
-        v.push(i);
+        v0.push(i);
     }
 
-    for j in v {
+    while c <= 12 {
+        v1.clear();
+
+        for j in &v0 {
+            v1.push(&j[c-1..c]);
+        }
+
+        let f0 = v1.iter().filter(|&n| *n == "0").count();
+        let f1 = v1.iter().filter(|&n| *n == "1").count();
+
+        if f0 > f1 { // change the order of the operator to get the other count
+            // snip
+        } else {
+            // snip
+        }
         
+        c += 1;
     }
 
 }
