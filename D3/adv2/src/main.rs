fa16f775abd568418017aa6b9763380425ac5ea9
Operaho <emptyloperaho@gmail.com>
2022-05-30 21:54:33 -0300

    working on it

--- D3/adv2/src/main.rs
+++ D3/adv2/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("/home/operaho/Advent/D3/adv.txt").unwrap();
+    let v = Vec::new();
+
+    for i in file.lines() {
+        v.push(i);
+    }
+
+    for j in v {
+        
+    }
+
 }
