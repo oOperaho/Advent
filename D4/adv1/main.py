a2685a77740bf45ffdeea0a9b33a9be716b38c5e
Operaho <emptyloperaho@gmail.com>
2022-06-05 22:47:34 -0300

    not even close

---
+++ D4/adv1/main.py
@@ -0,0 +1,7 @@
+f0 = open("/home/operaho/Advent/D4/adv.txt", "r")
+f1 = open("/home/operaho/Advent/D4/instructions.txt", "r")
+
+bingo, inst = f0.read().splitlines(), f1.read().splitlines()
+
+for i in bingo:
+	print(i.split("  "))
--- D4/adv1/src/main.rs
+++ D4/adv1/src/main.rs
@@ -2,11 +2,15 @@ use std::fs;
 
 fn main() {
     let bingo = fs::read_to_string("/home/operaho/Advent/D4/adv.txt").unwrap();
-    let inst = fs::read_to_string("/home/operaho/Advent/D4/instructions.txt").unwrap();
+    let f = fs::read_to_string("/home/operaho/Advent/D4/instructions.txt").unwrap();
+    let inst: Vec<&str> = f.split(",").collect();
     let mut v = Vec::new();
+    let mut c = 0;
 
     for i in bingo.lines() {
-        v.push([i])
+        if i != "" {
+            v.push([i])
+        }
     }
 
     println!("{:?}", v);
