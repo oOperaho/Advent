76a4c40f98712600d5f7d00c1d71eec369fbe0d9
oOperaho <Operaho@protonmail.com>
2022-09-04 21:49:00 -0300

    comment

--- D4/adv1/main.py
+++ D4/adv1/main.py
@@ -1,7 +1,10 @@
-f0 = open("/home/operaho/Advent/D4/adv.txt", "r")
-f1 = open("/home/operaho/Advent/D4/instructions.txt", "r")
+f0 = open("/home/operaho/Code/Advent/D4/adv.txt", "r")
+f1 = open("/home/operaho/Code/Advent/D4/instructions.txt", "r")
 
 bingo, inst = f0.read().splitlines(), f1.read().splitlines()
 
 for i in bingo:
-	print(i.split("  "))
+	print(i.split(" "))
+
+
+# snip
