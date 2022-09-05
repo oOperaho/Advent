fed11b23f3c745fd7818505a3b2a62e21eb9784c
oOperaho <Operaho@protonmail.com>
2022-09-04 22:38:53 -0300

    as

--- D4/adv1/main.py
+++ D4/adv1/main.py
@@ -1,11 +1,14 @@
 f0 = open("/home/operaho/Code/Advent/D4/adv.txt", "r")
 f1 = open("/home/operaho/Code/Advent/D4/instructions.txt", "r")
 
-bingo, inst, l = f0.read().splitlines(), f1.read(), []
+bingo, inst, l, fnl, c0 = f0.read().splitlines(), f1.read(), [], [], 0
 
 for i in bingo:
 	if i != '':
 		l.append(i)
 
 
-
+for j in range(0, len(l)):
+	
+	
+	c0 = c0 + 5
